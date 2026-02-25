33dbe8e1e6d7a59a5d700e89f7efdadded8446a1
Priyabrata Mondal <priyabratamondal1203@gmail.com>
2026-02-25 23:26:05 +0530

    2nd commit

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
+/*
+
+    HMR-Hot Module Reloading
+    File Watcher Algoritm- C++
+    Bundling
+    Minify
+    Cleaning our Code
+    Dev abd Production Build
+    Super Fast build Algo
+    Image optimization
+    Caching while development
+    compression
+    Compatibility with older version (polyfills)
+    Can create HTTPS on dev
+
+*/  
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
     "h1",
     {
-        id: "title",
+        id: "title1",
+        key: "1h",
     },
-    "Heading 1"
+    "Heading 1 teri mkc"
 );
 
 const heading2 = React.createElement(
     "h2",
     {
-        id: "title",
+        id: "title2",
+        key: "2h",
     },
     "Heading 2"
 );
